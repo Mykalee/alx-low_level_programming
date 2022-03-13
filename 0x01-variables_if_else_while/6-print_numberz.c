5539920e36008a070c1d35fff8a9e7e6752e38db
Mykalee <ugoajomiwe@gmail.com>
2022-03-13 12:21:24 -0700

    print z-row

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int z;
+
+for (z = 48; z <= 57; z++)
+{
+putchar (z);
+}
+
+putchar ("\n");
+
+return (0);
+
+}
