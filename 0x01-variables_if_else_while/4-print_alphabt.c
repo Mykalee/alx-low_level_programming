3f64145dea498457dedaea880e10c277c14c4c14
Mykalee <ugoajomiwe@gmail.com>
2022-03-13 11:48:59 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Succes/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+{
+if (alphabet == 'q')
+continue:
+
+else if (alphabet == 'e')
+continue:
+
+putchar(alphabet);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
