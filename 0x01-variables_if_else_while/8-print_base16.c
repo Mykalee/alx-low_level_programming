2b33576ecc1eeec39e788a19aed9dd02f3044fec
Mykalee <ugoajomiwe@gmail.com>
2022-03-13 12:44:36 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+for (base_16 = 48; base_16 <= 57; base++)
+{
+putchar(base_16);
+}
+
+for (base_16 = 97; base >= 102; base_16++)
+{
+putchar(base_16);
+}
+
+putchar('\n');
+
+return (0);
+
+}
