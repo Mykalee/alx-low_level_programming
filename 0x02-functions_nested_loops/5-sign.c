2a7a05691c055bfb3a990dd3510db7d51beeadb3
Mykalee <ugoajomiwe@gmail.com>
2022-03-16 10:50:43 -0700

    my commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * @n: the number to be checked.
  * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
  */
-int print_sign(int n);
+int main(void)
 
 {
 	int n;
@@ -13,6 +13,7 @@ int print_sign(int n);
 	if (n > 0)
 	{
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
