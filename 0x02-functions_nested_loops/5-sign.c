b75dd4b5a1514c4f64a62780a8a34a9fe1cc1855
Mykalee <ugoajomiwe@gmail.com>
2022-03-16 14:13:53 -0700

    commit sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,20 +4,20 @@
  * @n: the number to be checked.
  * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
  */
-int print_sign(int r)
+int print_sign(int n)
 
 {
-	if (r > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (r == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else if (r < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
